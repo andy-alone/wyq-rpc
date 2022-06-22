c97595e1a6ab982e01b7735aac104ff9a1bd0d56
wangyongqiang <136738914@qq.com>
2022-06-22 23:53:55 +0800

    feat:整合日志

--- wyq-rpc-core/src/main/java/com/wyq/rpc/core/remoting/netty/server/DefaultClass.java
+++ wyq-rpc-core/src/main/java/com/wyq/rpc/core/remoting/netty/server/DefaultClass.java
@@ -1,8 +1,15 @@
 package com.wyq.rpc.core.remoting.netty.server;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Author wyq
  * @Date 2022-06-19 23:11
  */
+@Slf4j
 public class DefaultClass {
+
+    public static void main(String[] args) {
+        log.info("eeeee");
+    }
 }
